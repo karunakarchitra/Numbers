b0d24c70db47f42c239500d354a2613abe95ab79
karunakarchitra <chitrakarunakar@gmail.com>
2023-05-31 15:09:40 +0530

    done

---
+++ src/com/sathya/Numbers.java
@@ -0,0 +1,13 @@
+package com.sathya;
+
+public class Numbers {
+		public static void main(String[] args) {
+			int num1=20;
+			int num2=10;
+			int res=num1+num2;
+			System.out.println("Addition of two number  " +res);
+		}
+
+	}
+
+
